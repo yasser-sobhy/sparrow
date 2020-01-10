52d960c3b26ccaa9239a6bebe558673990011796
yasser-sobhy <yasser.sobhy.net@gmail.com>
2020-01-10 03:46:44 +0200

    add log middleware

---
+++ middlewares/log.go
@@ -0,0 +1,41 @@
+package middlewares
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/yasser-sobhy/sparrow/core"
+)
+
+// This middleware log incomming tweets to a log file
+type Log struct {
+	Name         []byte
+	Enabled      bool
+	LogFullTweet bool
+}
+
+func NewLog() *Log {
+	viper.SetDefault("enabled", true)
+	viper.SetDefault("log_full_tweet", false)
+
+	return &Log{
+		Name:         []byte("Log"),
+		Enabled:      viper.GetBool("enabled"),
+		LogFullTweet: viper.GetBool("log_full_tweet"),
+	}
+}
+
+func (log *Log) Congregate(flock *core.Flock, options core.MiddlewareOptions) {
+	// TODO use a dedicated logger here not Sparrow::Logger
+	process := func(ws *core.WebSocket, user *core.User, tweet *core.Tweet) bool {
+		if log.LogFullTweet {
+			logrus.Info("New Tweet: {}{}", user.ID, tweet.Raw)
+		} else {
+			logrus.Info("New Tweet: {}{}", user.ID, tweet.Code)
+		}
+		return true
+	}
+
+	if log.Enabled {
+		flock.AddMiddleware(process, options)
+	}
+}

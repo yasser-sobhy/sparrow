164a8acebcfdfbbe22681217f2d16cc31048b25d
yasser-sobhy <yasser.sobhy.net@gmail.com>
2020-01-10 04:08:32 +0200

    seed basic auth

---
+++ auth/basic_auth.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/yasser-sobhy/sparrow/core"
+)
+
+// BasicAuth allows users to login using a username and password
+// RemoteAuth
+type BasicAuth struct {
+	sparrow *core.Sparrow
+	// users allowed to login
+	// read from configuration file or environment variables
+	userIDs map[string]string
+}
+
+// NewBasicAuth creates a new BasicAuth. Reads users data from config files
+func NewBasicAuth(sparrow *core.Sparrow) BasicAuth {
+	userIDs := viper.GetStringMapString("basic_auth.users")
+	return BasicAuth{userIDs: userIDs, sparrow: sparrow}
+}
+
+// Login processes the incoming tweet and allows user to login if user name and password
+// found in UserIDs map
+func (basicAuth *BasicAuth) Login(ws *core.WebSocket, user *core.User, tweet *core.Tweet) {
+	userInfo := tweet.Arguments[0]
+	if info, ok := basicAuth.userIDs[string(userInfo.Name)]; ok && info == string(userInfo.Value) {
+		logrus.Info("BasicAuth, loggin in user {}", userInfo.Name)
+		basicAuth.sparrow.LogUserIn(tweet.Arguments[0].Value, core.USER, ws)
+	} else {
+		logrus.Warn("BasicAuth, user not fount {}", userInfo.Name)
+		//ws.close()
+	}
+}

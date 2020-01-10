1e2a563b9e2bb19edb02c695e206c504927c4e78
yasser-sobhy <yasser.sobhy.net@gmail.com>
2020-01-10 03:19:20 +0200

    add env

---
+++ env/env.go
@@ -0,0 +1,17 @@
+package env
+
+type Env struct {
+	environment string
+}
+
+var env *Env
+
+func New() Env { return Env{"development"} }
+
+func Is(e string) bool { return env.environment == e }
+
+func IsDevelopment() bool { return env.environment == "development" }
+func IsStaging() bool     { return env.environment == "staging" }
+func IsProduction() bool  { return env.environment == "production" }
+
+func SetEnvironment(e string) { env.environment = e }

103042bc096f3928dd2667ea635e3856306ea59f
yasser-sobhy <yasser.sobhy.net@gmail.com>
2020-01-20 11:27:24 +0200

    fix middleware arg

--- sparrow.go
+++ sparrow.go
@@ -5,6 +5,6 @@ import "github.com/yasser-sobhy/sparrow/middlewares"
 
 func main() {
 	var sparrow core.Sparrow
-	sparrow.InstallMiddleware(middlewares.Debug{}, core.MiddlewareOptions{})
+	sparrow.InstallMiddleware(&middlewares.Debug{}, core.MiddlewareOptions{})
 	sparrow.Run()
 }

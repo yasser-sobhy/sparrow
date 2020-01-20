40167a1dfe1f9b526dfbaff94af70ca942b79eb8
yasser-sobhy <yasser.sobhy.net@gmail.com>
2020-01-20 13:59:17 +0200

    add example twitter

--- sparrow.go
+++ sparrow.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/yasser-sobhy/sparrow/core"
-import "github.com/yasser-sobhy/sparrow/middlewares"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/yasser-sobhy/sparrow/core"
+	"github.com/yasser-sobhy/sparrow/middlewares"
+)
 
 func main() {
-	var sparrow core.Sparrow
+	var sparrow *core.Sparrow = core.NewSparrow()
 	sparrow.InstallMiddleware(&middlewares.Debug{}, core.MiddlewareOptions{})
+
+	sparrow.Use(func(c *core.Conn, u *core.User, t *core.Tweet) bool {
+		logrus.Info(string(t.Code), string(t.Content))
+		c.WriteMessage([]byte("Hello Sparrow!"))
+		return true
+	}, core.TwitterOptions{Code: '0', Scope: core.NONE})
 	sparrow.Run()
 }

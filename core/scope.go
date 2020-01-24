6bc0367bd4a8d0be40cc6f0bd407399262774e85
yasser-sobhy <yasser.sobhy.net@gmail.com>
2020-01-24 16:35:35 +0200

    refactor scopes

--- core/scope.go
+++ core/scope.go
@@ -3,9 +3,8 @@ package core
 type Scope int32
 
 const (
-	// rename ANY to ALL, or NONE to ANONYMOUS ?
-	ANY   Scope = 0 // all scopes
-	NONE  Scope = 1 // before loging in
+	NONE  Scope = 0 // before loging in
+	ANY   Scope = 1 // all scopes
 	USER  Scope = 2 // a user
 	ADMIN Scope = 3 // an admin
 )
